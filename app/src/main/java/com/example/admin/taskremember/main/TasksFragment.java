ddb1ce3552cabce6316a260f9ff4f00aed831a8d
borserb <borserb@gmail.com>
2019-03-29 17:22:25 +0200

    lesson 6 29.03.19

--- app/src/main/java/com/example/admin/taskremember/main/TasksFragment.java
+++ app/src/main/java/com/example/admin/taskremember/main/TasksFragment.java
@@ -8,6 +8,7 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -35,7 +36,7 @@ public class TasksFragment extends Fragment {
     public final static int ACTIVITY_CODE = 101;
 
     private SwipeRefreshLayout swipeRefreshLayout;
-    private ImageButton imageButton;
+    private FloatingActionButton imageButton;
     private List <Task> tasks = new ArrayList();
     private ConstraintLayout backgorund;
     private RecycleViewAdpter adapter;
